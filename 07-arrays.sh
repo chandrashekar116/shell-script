310c0d9ea7bf4d372f1157a7e883ed74d23c039c
unknown <chandrashekar116>
2023-12-14 03:00:44 +0530

    Updated Shell script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -3,10 +3,10 @@
 FRUITS=("APPLE" "BANANA" "GUAVA")
 
 
-echo "First Value: $(FRUITS[0])" 
+echo "First Value: ${FRUITS[0]}" 
 
-echo "second Value: $(FRUITS[1])" 
+echo "second Value: ${FRUITS[1]}" 
 
-echo "Third Value: $(FRUITS[2])" 
+echo "Third Value: ${FRUITS[2]}" 
 
 
