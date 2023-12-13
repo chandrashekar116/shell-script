cf68f3bd19d3459a8a1eb47feac62012c15b46dc
unknown <chandrashekar116>
2023-12-14 01:53:44 +0530

    Updated Shell script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-PERSON1=Ramesh
-PERSON2=Suresh
+PERSON1=Ram
+PERSON2=Raheem
 
 
 
 echo "$PERSON1":: Hello $PERSON2, Good Morning"
  "$PERSON2":: Hello $PERSON1, Very Good Morning"
+
+
 
 
