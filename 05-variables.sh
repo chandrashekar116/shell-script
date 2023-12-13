f391e8c57f19f169799a5d1422e1a8fa5b5376bc
unknown <chandrashekar116>
2023-12-14 02:36:09 +0530

    Updated Shell script

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,4 +4,10 @@ echo "Please enter Username:"
 
 read -s USERNAME
  
-echo "Username is:  $USERNAME"
+echo "Username is:  $PASSWORD"
+
+echo "Please enter PASSWORD:"
+
+read -s PASSWORD
+ 
+echo "PASSWORD is:  $PASSWORD"
