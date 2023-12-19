935779ba2e1ea4d594d65bad936b4f50cc4b6906
unknown <chandrashekar116>
2023-12-19 10:30:41 +0530

    Shell scripting

--- 12-install-packages.sh
+++ 12-install-packages.sh
@@ -3,8 +3,22 @@
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
 
+TIMESTAMP=$(date +%-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE 
+
+VALIDATE(){
+if [ $1 -NE 0]
+    then
+    echo -e "$2... $R FAILED $N"
+    else
+        echo -e "$2... $G SUCCESS $N"
+
+}
 if [ $ID -ne 0 ]
 then
 echo -e "$R ERROR:: Please run this script with root access $N"
@@ -13,4 +27,19 @@ else
 echo "You are root user"
 fi 
 
-echo "All arguments passed: $@"
+#echo "All arguments passed: $@"
+#for package in $@ #package=git
+
+
+for package in $@
+do
+yum list installed $package &>> $LOGFILE #check installed or not 
+ if [ $? -ne 0 ]  #if not installed
+then 
+yum install $package -y &>> $LOGFILE  #install the package
+VALIDATE $? "INSTALLATION OF $package"  #validate
+
+else
+echo -e "$package is already installed.... $Y Skipping $N"
+fi
+done
